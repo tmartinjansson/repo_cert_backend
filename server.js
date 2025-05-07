7b95f36125b9fa53a3d99fcd1267c6d93a1e521b
MartinJansson <tom.martin1809@gmail.com>
2025-05-07 17:01:38 +0200

    vercel backend

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["https://certification-pink.vercel.app", "http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // MongoDB Connection
